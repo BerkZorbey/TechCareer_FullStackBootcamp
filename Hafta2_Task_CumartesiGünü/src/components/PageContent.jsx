a8ffca69fa38b83a466605807c6fee05d07ebd6c
Taha Berk YEŞİLALİOĞLU <63355948+BerkZorbey@users.noreply.github.com>
2023-10-14 12:12:28 +0300

    Add files via upload

---
+++ Hafta2_Task_CumartesiGünü/src/components/PageContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import About from './About'
+import Project from './Project'
+import Contact from './Contact'
+import ImageOfLocation from './ImageOfLocation'
+
+function PageContent() {
+  return (<>
+    <Project></Project>
+    <About></About>
+    <Contact></Contact>
+    <ImageOfLocation></ImageOfLocation>
+  </>
+  )
+}
+
+export default PageContent
